0479e97d29b5dc8556c2c1a2d06b8e7a81d1147c
Mario-arch <mariopersonalweb@gmail.com>
2026-02-22 11:47:19 +0100

    add less alias

--- zsh/.oh-my-zsh-custom/functions.zsh
+++ zsh/.oh-my-zsh-custom/functions.zsh
@@ -1,49 +1,54 @@
 # Install package
 pac() {
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		echo "pac: Install package"
+		return 0
+	fi
     local pkgs
     pkgs=$(pacman -Ss | fzf -m | awk '{print $1}' | cut -d'/' -f2) || return 0
     [ -n "$pkgs" ] && sudo pacman -S $pkgs
-	if [[ $1 -eq "-h" || $1 -eq "--help" ]]; then
-		echo "pac: Install package"
-	fi
 }
 
 # Search info about a package
 pacs() {
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		echo "pacs: Search info about a remote package"
+		return 0
+	fi
     local pkgs
     pkgs=$(pacman -Ss | fzf -m | awk '{print $1}' | cut -d'/' -f2) || return 0
     [ -n "$pkgs" ] && pacman -Si $pkgs
-	if [[ $1 -eq "-h" || $1 -eq "--help" ]]; then
-		echo "pacs: Search info about a remote package"
-	fi
 }
 
 # Remove package
 pacrm() {
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		echo "pacrm: Remove package"
+		return 0
+	fi
     local pkgs
     pkgs=$(pacman -Qq | fzf -m) || return 0
     [ -n "$pkgs" ] && sudo pacman -Rns $pkgs
-	if [[ $1 -eq "-h" || $1 -eq "--help" ]]; then
-		echo "pacrm: Remove package"
-	fi
 }
 
 # Get info about installed package
 pacq() {
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		echo "pacq: Query info about a local package"
+		return 0
+	fi
     local pkgs
     pkgs=$(pacman -Qq | fzf -m) || return 0
     [ -n "$pkgs" ] && pacman -Qikk $pkgs
-	if [[ $1 -eq "-h" || $1 -eq "--help" ]]; then
-		echo "pacq: Query info about a local package"
-	fi
 }
 
 # Get info about explicitly installed package
 pacqe() {
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		echo "pacqe: Query info about a local, explicitly installed, package"
+		return 0
+	fi
     local pkgs
     pkgs=$(pacman -Qqe | fzf -m) || return 0
     [ -n "$pkgs" ] && pacman -Qikk $pkgs
-	if [[ $1 -eq "-h" || $1 -eq "--help" ]]; then
-		echo "pacqe: Query info about a local, explicitly installed, package"
-	fi
 }
